7b36620201f2748d6854d856491715410385b978
ishpartko <ishpartko@gmail.com>
2019-06-10 16:45:06 +0300

    update structure

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,24 @@
 <template>
-  <div id="app">
+  <div class="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/create">Создать игру</router-link> |
+      <router-link to="/join">Присоединиться</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+import 'normalize.css';
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {}
+</script>
+
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+  padding: 10px;
 }
 </style>
---
+++ src/views/CreateGame.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="settings">
+    <label class="settings__label">
+      Название игры 
+      <input type="text" placeholder="Случайное название" v-model="gameName">
+    </label>
+    <label class="settings__label">
+      Количество попыток для угадывания каждому игроку 
+      <input type="number" v-model="tryCount">
+    </label>
+    <label class="settings__label"> 
+      Дополнительные правила для ваших игроков 
+      <textarea v-model="additionalRules" disabled/>
+    </label>
+    <label class="settings__label">
+       Пожелания игрокам перед игрой
+      <textarea v-model="additionalWishes" disabled/>
+    </label>
+    <label class="settings__label">
+       Поздравление победителя
+      <textarea v-model="winnerCongratulateMsg" disabled/>
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CreateGame extends Vue {
+  public gameName: string = '';
+  public tryCount: number = 3;
+  public additionalRules: string = '';
+  public additionalWishes: string = 'Игра началась!';
+  public winnerCongratulateMsg: string = 'Поздравляем победителя!';
+}
+</script>
+
+<style scoped>
+.settings {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.settings__label {
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0; 
+}
+</style>
