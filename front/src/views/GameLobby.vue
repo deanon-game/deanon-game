fbe22b3ca29eb0a6512b2ffa27296236fa27654a
Degebery <Degebery@gmail.com>
2019-07-13 12:29:20 +0300

    added realtime update

--- front/src/views/GameLobby.vue
+++ front/src/views/GameLobby.vue
@@ -6,38 +6,29 @@
 
 <script>
 import axios from 'axios'
-// import io from 'socket.io-client'
+import io from 'socket.io-client'
 
 export default {
   data () {
     return {
-      gameInfo: {}
+      gameInfo: {},
+      error: ''
     }
   },
   created () {
-  //  const socket = io('http://localhost:8000')
-  //  socket.emit('join room', 'Pitoshka', 'eNwGmea1561096180238')
-  //  socket.on('new data', doc => {
-  //    console.log(doc)
-  //  })
+    const socket = io('http://localhost:8000')
+    socket.emit('join room', localStorage.token, 'eNwGmea1561096180238')
+    socket.on('new data', doc => {
+      this.gameInfo = doc.params
+    })
+    socket.on('err', data => {
+      this.error = data
+      console.log(this.error)
+    })
   },
   methods: {
-    getGameInfo () {
-      return new Promise((resolve) => {
-        axios.get(`${this.$store.getters.getAPI_URL}game/${this.$route.params.id}/info`)
-          .then((response) => {
-            const gameInfo = response.data.token
-            if (!gameInfo) {
-              this.errorList.push('Сервер не вернул данные игры :(')
-            } else {
-              resolve(gameInfo)
-            }
-          })
-          .catch((e) => {
-            this.errorList = []
-            this.errorList.push(e)
-          })
-      })
+    loadPlayer () {
+      axios.post(`${this.$store.getters.getAPI_URL}games/loadPlayer`)
     }
   }
 }
--- server/socket/index.js
+++ server/socket/index.js
@@ -3,10 +3,16 @@ const dbCtrl = require('../controllers/dbCtrl')
 module.exports = function (io) {
   io.on('connection', (socket) => {
     console.log('Someone Connected')
-    socket.on('join room', (nick, room) => {
-      socket.join(room)
-      console.log(nick + ' joined to: ' + room)
-      dbCtrl.onUpdate(room, io)
+    socket.on('join room', (token, room) => {
+      dbCtrl.checkToken(token, room).then(isValid => {
+        if (!isValid) {
+          io.to(socket.id).emit('err', 'token is not valid')
+          return
+        }
+        console.log('joined')
+        socket.join(room)
+        dbCtrl.signUpdate(room, io)
+      })
     })
   })
 }
