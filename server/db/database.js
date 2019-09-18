727e633a279d1f3f5638db0dfe1ee57485c718d9
ishpartko <ishpartko@gmail.com>
2019-09-19 02:29:47 +0300

    delete unused back

--- server/db/database.js
+++
@@ -1,9 +0,0 @@
-const config = require('./fb-config.js')
-const firebase = require('firebase')
-require('firebase/firestore')
-
-firebase.initializeApp(config)
-const db = firebase.firestore()
-
-exports.firebase = firebase
-exports.db = db
