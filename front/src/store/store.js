4e5ed471614ec7b973adf02fd4ae74b40ee2a9d9
ishpartko <ishpartko@gmail.com>
2019-09-19 02:34:11 +0300

    create app structure

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,21 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import client from 'client/index'
+import server from 'server/index'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    API_URL: `http://localhost:8000/api/v1/`
-  },
-  getters: {
-    getAPI_URL (state) {
-      return state.API_URL
-    }
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    client,
+    server
   }
 })
