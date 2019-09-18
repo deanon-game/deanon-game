ee0c6fb1ee9bf3bca126205888466f5b35876d22
ishpartko <ishpartko@gmail.com>
2019-09-19 02:28:58 +0300

    update proxy

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   devServer: {
-    proxy: 'http://localhost:8000'
+    hot: true, // https://webpack.js.org/configuration/dev-server/#devserverhot
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:8000/api/',
+        ws: true,
+        changeOrigin: true
+      }
+    }
   }
 }
