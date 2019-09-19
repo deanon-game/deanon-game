bf2d66f53d29609d4b7ce397d0bf3c9c41995e7a
ishpartko <ishpartko@gmail.com>
2019-09-20 00:40:45 +0300

    add server store

--- front/src/store/server/server.js
+++ front/src/store/server/server.js
@@ -1,7 +1,32 @@
+import Peer from 'peerjs'
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  getters: {},
-  actions: {}
+  state: {
+    peer: {}
+  },
+  mutations: {
+    peer (state, value) {
+      state.peer = value
+    }
+  },
+  getters: {
+    peer (state) {
+      return state.peer
+    },
+    peerId (state) {
+      return state.peer.id
+    }
+  },
+  actions: {
+    create (state) {
+      const peer = new Peer('flsdkjflksjd', { host: '/p2p/' })
+      peer.on('connection', (conn) => {
+        conn.on('data', (data) => {
+          console.log(data)
+        })
+      })
+      state.commit('peer', peer)
+    }
+  }
 }
