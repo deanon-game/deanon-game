e3db40c4d177e63c5ef0326b38a2de555ad829be
ishpartko <ishpartko@gmail.com>
2019-09-22 02:08:49 +0300

    proxy not working

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   devServer: {
-    hot: true, // https://webpack.js.org/configuration/dev-server/#devserverhot
     proxy: {
       '/api/p2p/': {
         target: 'http://localhost:9000/api/p2p/',
