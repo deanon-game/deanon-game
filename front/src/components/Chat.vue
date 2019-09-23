02b4a741c4bbf369d8acbd0e8141da75a5dcbb51
ishpartko <ishpartko@gmail.com>
2019-09-23 14:39:33 +0300

    add svg import for ts

--- front/src/components/Chat.vue
+++ front/src/components/Chat.vue
@@ -1,31 +1,31 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="400"
-    tile
-  >
-    <v-list avatar three-line>
+    <v-list avatar flat disabled three-line>
       <v-list-item-group>
         <v-list-item>
           <v-list-item-avatar>
-            <!-- <v-img src="fsdkljf"></v-img> -->
+            <img alt="user logo" :src="logo" />
             аватар
           </v-list-item-avatar>
           <v-list-item-content>
-            <v-list-item-title>flsdkjfs</v-list-item-title>
-            <v-list-item-subtitle>f;slldkf;lksd fsdjlfksjd lfkjs dlkjflsdkjflksdjf</v-list-item-subtitle>
+            <v-list-item-title></v-list-item-title>
+            <v-list-item-subtitle></v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
     </v-list>
-  </v-card>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({
+import logo from '../assets/anonymous.svg'
 
+export default Vue.extend({
+  data: function () {
+    return {
+      logo: logo
+    }
+  }
 })
 </script>
 
