bc566bc093dd16bed2e90efbdbee0988686bfe2b
ishpartko <ishpartko@gmail.com>
2019-09-28 03:59:32 +0300

    update joinchat component

---
+++ front/src/components/JoinChat.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <v-container>
+    <v-btn @click="onJoin">
+      Присоединиться
+    </v-btn>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import ChatMessage from './ChatMessage.vue'
+
+@Component({
+  components: {
+    ChatMessage
+  }
+})
+export default class JoinChat extends Vue {
+  @Prop({
+    type: String,
+    required: true
+  }) readonly id!: string
+
+  created () {
+    this.joinChat()
+  }
+  joinChat () {
+    this.$store.dispatch('client/connect', this.id)
+  }
+  onJoin () {
+    this.$store.dispatch('client/send', {
+      type: 'chat',
+      message: 'hello :)'
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
