34ab0308976b4d60de7b4e6d3847205042d43eaa
ishpartko <ishpartko@gmail.com>
2019-09-29 21:39:14 +0300

    change vuex to dunamic modules

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,13 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import client from './client/index.js'
-import server from './server/index'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  modules: {
-    client,
-    server
-  }
-})
+export default new Vuex.Store({})
