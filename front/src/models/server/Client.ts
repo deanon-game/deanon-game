9181b8e01556bdf1e55f5b28323765f9e9af4358
ishpartko <ishpartko@gmail.com>
2019-09-30 01:56:50 +0300

    update params type

--- front/src/models/server/Client.ts
+++ front/src/models/server/Client.ts
@@ -3,11 +3,13 @@ import ModuleRequest from '@/models/common/ModuleRequest'
 import Connection from '@/models/common/Connection'
 import FreeObject from '@/models/common/FreeObject'
 
-export type NewUserRequest = ModuleRequest<{name: string}, FreeObject>
+export interface NewUserDataParams {
+  name: string
+}
 
 export default class Client extends User {
   public connection: Connection
-  constructor (request: NewUserRequest) {
+  constructor (request: ModuleRequest<NewUserDataParams, FreeObject>) {
     super({ name: request.data.params.name })
     this.connection = request.connection
   }
