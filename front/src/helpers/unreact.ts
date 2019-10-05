b193e19823757cff92a6d11f31ca46fad9e16876
ishpartko <ishpartko@gmail.com>
2019-10-06 00:28:10 +0300

    update unreact function

--- front/src/helpers/unreact.ts
+++ front/src/helpers/unreact.ts
@@ -1,3 +1,20 @@
+const getCircularReplacer = () => {
+  const seen = new WeakSet()
+  return (key: string, value: any) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return
+      }
+      seen.add(value)
+    }
+    return value
+  }
+}
+
 export default function (payload: any) {
-  return JSON.parse(JSON.stringify(payload))
+  try {
+    return JSON.parse(JSON.stringify(payload, getCircularReplacer()))
+  } catch (error) {
+    console.error(error)
+  }
 }
