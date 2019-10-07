f93cd1acf9e789cc80de23d89aa1ea2413356cdd
ishpartko <ishpartko@gmail.com>
2019-10-08 02:10:15 +0300

    add login form

--- front/src/components/JoinChat.vue
+++ front/src/components/JoinChat.vue
@@ -1,7 +1,11 @@
 
 <template>
   <v-container>
-    <Chat />
+    <Chat v-if="isLogined" />
+    <LoginForm
+      v-else
+      :is-joined-to-server="isJoined"
+    />
   </v-container>
 </template>
 
@@ -10,13 +14,16 @@ import { Vue, Component, Prop } from 'vue-property-decorator'
 import ChatMessage from './ChatMessage.vue'
 
 import ClientModule, { ConnectionPayload } from '@/store/modules/client-core'
+import ClientChatModule from '@/store/modules/client-chat'
 import { IClientRequest } from '@/models/client/ClientRequest'
 import Chat from '@/components/Chat.vue'
+import LoginForm from '@/components/LoginForm.vue'
 
 @Component({
   components: {
     ChatMessage,
-    Chat
+    Chat,
+    LoginForm
   }
 })
 export default class JoinChat extends Vue {
@@ -25,27 +32,22 @@ export default class JoinChat extends Vue {
     required: true
   }) readonly serverId!: string
 
+  get isLogined () {
+    return ClientChatModule.isLogined
+  }
+
   created () {
     this.joinChat()
   }
+
+  private isJoined: boolean = false
   joinChat () {
     const payload: ConnectionPayload = {
       serverId: this.serverId
     }
     ClientModule.connect(payload).then(() => {
-      this.onJoin()
+      this.isJoined = true
     })
-  }
-  onJoin () {
-    const data: IClientRequest = {
-      query: 'server/auth?renameMe',
-      data: {
-        params: {
-          name: 'называйте меня ИВАН'
-        }
-      }
-    }
-    ClientModule.send(data)
   }
 }
 </script>
