19e307e2658bd7cbb397b4ecb352937ccebaa743
ishpartko <ishpartko@gmail.com>
2020-06-11 12:58:23 +0300

    update layout

--- front/src/views/MainPage.vue
+++ front/src/views/MainPage.vue
@@ -1,5 +1,15 @@
 <template>
-  <main>
+  <main class="main">
+    <section class="intro">
+      <h1>
+        Pygmalemon 🍋 – безопасный чат.
+      </h1>
+      <img
+        width="100%"
+        src="@/assets/stockfresh_id1508151_6c9f7f.jpg"
+        class="intro__image"
+      >
+    </section>
     <section>
       <v-btn :to="{name:'create'}">
         Создать комнату
@@ -13,3 +23,27 @@ export default {
   name: 'MainPage'
 }
 </script>
+
+<style scoped>
+.main {
+  display: flex;
+  align-items: center;
+
+  flex-direction: column;
+  justify-content: center;
+
+  min-height: 90vh;
+}
+.intro {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.intro__image {
+  object-fit: contain;
+  min-height: 300px;
+  max-width: 600px;
+}
+</style>
