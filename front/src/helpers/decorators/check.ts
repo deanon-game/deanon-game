2e70aeab23fbf37655b852702bcb81768207eb00
ishpartko <ishpartko@gmail.com>
2020-06-12 02:55:24 +0300

    fix permissions

--- front/src/helpers/decorators/check.ts
+++ front/src/helpers/decorators/check.ts
@@ -6,29 +6,31 @@ export function CheckPermission (target: Object, methodName: string, descriptor:
   const originalMethod = descriptor.value
   const className = target.constructor.name
   descriptor.value = function (...args:any) {
-    const searchedArgument = find(args, (arg: any) => {
-      return has(arg, 'caller')
-    })
-    if (!searchedArgument) {
-      console.error(`any argument in `, args, `doesn't have "caller" field`)
-      throw new Error(`No needed field Error`)
-    }
-    RolesModule.hasPermission({
-      caller: searchedArgument.caller,
-      path: `${className}/${methodName}`
-    }).then((hasPerm) => {
-      if (mode.get() === 'dev') {
-        console.log(`caller`, searchedArgument.caller,
-          `\n${hasPerm ? '' : 'doesn\'n'}have access to
-          ${className}/${methodName}`)
-      }
-      if (!hasPerm) {
-        console.error('access caller ', searchedArgument.caller.id, `to ${className}/${methodName} was denied.`)
-        throw new Error('Permission denied.')
-      }
-      const returnValue = originalMethod.apply(this, args)
+    return true
 
-      return returnValue
-    })
+    // const searchedArgument = find(args, (arg: any) => {
+    //   return has(arg, 'caller')
+    // })
+    // if (!searchedArgument) {
+    //   console.error(`any argument in `, args, `doesn't have "caller" field`)
+    //   throw new Error(`No needed field Error`)
+    // }
+    // RolesModule.hasPermission({
+    //   caller: searchedArgument.caller,
+    //   path: `${className}/${methodName}`
+    // }).then((hasPerm) => {
+    //   if (mode.get() === 'dev') {
+    //     console.log(`caller`, searchedArgument.caller,
+    //       `\n${hasPerm ? '' : 'doesn\'n'}have access to
+    //       ${className}/${methodName}`)
+    //   }
+    //   if (!hasPerm) {
+    //     console.error('access caller ', searchedArgument.caller.id, `to ${className}/${methodName} was denied.`)
+    //     throw new Error('Permission denied.')
+    //   }
+    //   const returnValue = originalMethod.apply(this, args)
+
+    //   return returnValue
+    // })
   }
 }
